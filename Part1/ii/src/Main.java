3ccaadf09718c335918180152f13d2c54d547f05
Liam Breeze <liamkbreeze@gmail.com>
2018-08-15 17:07:31 +0100

    Add solution to Part 1.ii. "Draw a horizontal line".
    Objective was "Given a number n, print n asterisks on one line".

---
+++ Part1/ii/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args)
+    {
+        int lineLength = Integer.parseInt(args[0]);
+
+        for (int asteriskCount = 0; asteriskCount < lineLength; asteriskCount++)
+        {
+            System.out.print("*");
+        }
+
+        System.out.println();
+    }
+}
